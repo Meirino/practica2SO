48ccf9f182a616c1839dc404c2450fa5312f6530
Meirino <jjmeiras@gmail.com>
2017-11-08 10:35:47 +0100

    inicializacion

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv) {
+	int chid_pid;
+	int status;
+	char buff[1024];
+
+	printf("msh> ");
+	while(fgets(buff, 1024, stdin) != NULL) {
+		
+		printf("msh> ");
+	};
+}
